4cc8f1fc5aaba8f09cdbb58bca4651d82667cc52
Bobolay <bogdan4uk@gmail.com>
2016-04-13 18:41:53 +0300

    collections

--- app/assets/javascripts/appeared.js
+++ app/assets/javascripts/appeared.js
@@ -1,7 +1,6 @@
 $(function() {
-  var $appeared = $('.appear1,.appear2,.appear3,.appear4');
+  var $appeared = $('.appear1, .appear2, .appear3, .appear4, .first-door, .second-door, .third-door');
   $appeared.appear();
-
   $(document.body).on('appear', function(e, $affected) {
     $affected.addClass('appeared')
   });

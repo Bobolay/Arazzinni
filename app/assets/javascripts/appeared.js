f37d01e4b049579c79252d3c1be60861915211a8
Bobolay <bogdan4uk@gmail.com>
2016-04-14 11:35:39 +0300

    zzz

--- app/assets/javascripts/appeared.js
+++ app/assets/javascripts/appeared.js
@@ -1,5 +1,5 @@
 $(function() {
-  var $appeared = $('.appear1, .appear2, .appear3, .appear4, .first-door, .second-door, .third-door');
+  var $appeared = $('.appear1, .appear2, .appear3, .appear4, .appear5, .appear6, .appear7, .appear8, .appear9, .first-door, .second-door, .third-door');
   $appeared.appear();
   $(document.body).on('appear', function(e, $affected) {
     $affected.addClass('appeared')

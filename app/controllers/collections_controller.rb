1115ce944fc006a40fd347899ea372e473b9cd5f
pasha <p.korenev@voroninstudio.eu>
2016-09-14 14:46:22 +0300

    collections

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,4 +1,19 @@
 class CollectionsController < ApplicationController
+  before_action :set_collection, only: [:show]
+
+  def index
+    @collections = Collection.published.sort_by_sorting_position
+  end
+
   def show
+    @collections = Collection.published.sort_by_sorting_position
+  end
+
+  def set_collection
+    collection_id = params[:id]
+    @collection = Collection.published.joins(:translations).where(collection_translations: { locale: I18n.locale, url_fragment: collection_id }).first
+    if @collection.nil?
+      render_not_found
+    end
   end
 end

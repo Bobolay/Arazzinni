f916e434cfbd17cd7f44a0759558b0b7ccf16c59
pasha <p.korenev@voroninstudio.eu>
2016-10-06 13:46:34 +0300

    remove index

--- db/migrate/20161005155437_create_product_color_variants.rb
+++ db/migrate/20161005155437_create_product_color_variants.rb
@@ -1,8 +1,8 @@
 class CreateProductColorVariants < ActiveRecord::Migration
   def change
     create_table :product_color_variants do |t|
-      t.belongs_to :product, index: true, foreign_key: true
-      t.belongs_to :product_color, index: true, foreign_key: true
+      t.belongs_to :product#, index: true, foreign_key: true
+      t.belongs_to :product_color#, index: true, foreign_key: true
       t.attachment :avatar
 
       t.timestamps null: false
